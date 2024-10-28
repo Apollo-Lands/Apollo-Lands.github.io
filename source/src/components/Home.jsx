1230db90bde42d2a69665c331da19feb3b7666e5
Roy Watson-Badell <rwatsonbadell@roys-mbp.mynetworksettings.com>
2024-10-28 17:02:14 -0400

    fixing image bugs

--- source/src/components/Home.jsx
+++ source/src/components/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
         <div className="card-container">
           <section className="card">
             <h3>Homesite: Build Your Dream</h3>
-            <img className="card-image" src="/public/random farm house.jpg" alt="Homesite" />
+            <img className="card-image" src="/random farm house.jpg" alt="Homesite" />
             <div className="card-content">
               <p>Whether you’re dreaming of a secluded homestead, a tiny house getaway, or an Airbnb rental, land provides the perfect foundation.</p>
               <ul>
@@ -33,7 +33,7 @@ export default function Home() {
   
           <section className="card">
             <h3>Recreation: Your Playground</h3>
-            <img className="card-image" src="/public/womand_dog_tent.jpg" alt="Recreation" />
+            <img className="card-image" src="public/womand_dog_tent.jpg" alt="Recreation" />
             <div className="card-content">
               <p>Land isn’t just a place to live; it’s also a place to play.</p>
               <ul>
@@ -46,7 +46,7 @@ export default function Home() {
   
           <section className="card">
             <h3>Investment: Secure Your Future</h3>
-            <img className="card-image" src="/public/rising_hand_stock.jpg" alt="Investment" />
+            <img className="card-image" src="/rising_hand_stock.jpg" alt="Investment" />
             <div className="card-content">
               <p>Land isn’t just a purchase; it’s an investment. Like any other asset, land can appreciate in value over time.</p>
               <ul>
@@ -60,7 +60,7 @@ export default function Home() {
   
           <section className="card">
             <h3>Commercial: Build Your Business</h3>
-            <img className="card-image" src="public/commercial_investment_image.jpg" alt="Commercial Development" />
+            <img className="card-image" src="/commercial_investment_image.jpg" alt="Commercial Development" />
             <div className="card-content">
               <p>The right piece of land can be the starting point for a profitable business venture.</p>
               <ul>
